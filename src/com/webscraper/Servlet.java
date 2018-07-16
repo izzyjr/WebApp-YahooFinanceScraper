366e30069c22a6a444341b07b9525a6a1f14a763
Israel Mesa <mesa.israel50@gmail.com>
2018-07-15 21:11:27 -0400

    Controller(Servlet)

---
+++ src/com/webscraper/Servlet.java
@@ -0,0 +1,54 @@
+package com.webscraper;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<Coin> coins = CoinDataUtil.getCoins();
+		
+//		for (int i = 0; i < coins.size(); i++) {
+//            System.out.println(coins.get(i).getNumberOrder() + ": " + coins.get(i).getName());
+//        }
+		
+		request.setAttribute("coin_list", coins);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("view_table.jsp");
+		
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

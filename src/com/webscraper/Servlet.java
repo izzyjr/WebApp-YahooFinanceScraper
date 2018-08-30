eea175234adbe26dbe4a4d15944aa35c00c9b993
Israel Mesa <mesa.israel50@gmail.com>
2018-08-30 11:32:08 -0400

    update

--- src/com/webscraper/Servlet.java
+++ src/com/webscraper/Servlet.java
@@ -3,12 +3,14 @@ package com.webscraper;
 import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
+import javax.annotation.Resource;
+//import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class Servlet
@@ -16,31 +18,48 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Servlet")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Servlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+       	
+		private CoinDataUtil coinDataUtil;
+	
+		@Resource(name="WebApp-YahooFinanceScraper")
+		private DataSource dataSource;
+		
+		@Override
+		public void init() throws ServletException {
+			super.init();
+			
+			try {
+				coinDataUtil = new CoinDataUtil(dataSource);
+			}
+			catch (Exception exc) {
+				throw new ServletException(exc);
+			}
+		}
+		
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		List<Coin> coins = CoinDataUtil.getCoins();
+		List<Coin> coins = coinDataUtil.coinScraper();
+				
+		try {
+			coinDataUtil.listToDbTable(coins);
+		} catch (Exception exc) {
+			throw new ServletException(exc);
+		}
+
 		
 		for (int i = 0; i < coins.size(); i++) {
             System.out.println(coins.get(i).getNumberOrder() + ": " + coins.get(i).getName());
         }
-		
-		request.setAttribute("coin_list", coins);
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher("view_table.jsp");
-		
-		dispatcher.forward(request, response);
+//		
+//		request.setAttribute("coin_list", coins);
+//		
+//		RequestDispatcher dispatcher = request.getRequestDispatcher("view_table.jsp");
+//		
+//		dispatcher.forward(request, response);
 	}
 
 	/**
